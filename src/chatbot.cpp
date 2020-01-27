de868da9dd7c45b4a2dab48f1f011b599b9d4418
Julius <julius@e248-202.eduroam.tuwien.ac.at>
2020-01-27 11:18:22 +0100

    a few small fixes

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -27,7 +27,7 @@ ChatBot::ChatBot(std::string filename)
     _rootNode = nullptr;
 
     // load image into heap memory
-    _image.reset(new wxBitmap(filename, wxBITMAP_TYPE_PNG));
+    _image = std::make_unique<wxBitmap>(filename, wxBITMAP_TYPE_PNG);
 }
 
 ChatBot::~ChatBot()
--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -117,7 +117,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     //// STUDENT CODE
     ////
     // create chat logic instance
-    _chatLogic.reset(new ChatLogic()); 
+    _chatLogic = std::make_unique<ChatLogic>(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
